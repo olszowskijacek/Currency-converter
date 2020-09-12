eac249fb54aaa046aa2051be647185f3ba918de6
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-09-12 12:51:42 +0200

    select component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Button } from './components/Button';
+// import { Button } from './components/Button';
 import {Calculator} from './components/Calculator';
 const day = 'Saturday'
 
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// state = {
+//     amount: 0,
+//     currencyFrom: 'PLN',
+//     currencyTo: 'USD'
+// }
+// this.setState({ currencyFrom: event.target.value}, () => {
+//aktualny stan aplikacji
+//})
+function Select( {value, setCurrency}) {
+    return (
+        <select value={value} onChange={(event) => setCurrency(event.target.value)}>
+            <option value='USD'>USD</option>
+            <option value='PLN'>PLN</option>
+            <option value='GBP'>GBP</option>
+            <option value='EUR'>EUR</option>
+        </select>
+    );
+}
 
 function Calculator() {
+    const [amount, setAmount] = useState(0);
+    const [currencyFrom, setCurrencyFrom] = useState('PLN');
+    const [currencyTo, setCurrencyTo] = useState('USD');
     return (
         <form>
-        <div>
-          <input type='number' placeholder='Amount'></input>
-        </div>
-        <div>
-          <span>From: </span>
-          <select>
-            <option value='USD'>USD</option>
-            <option value='PLN'>PLN</option>
-            <option value='GBP'>GBP</option>
-            <option value='EUR'>EUR</option>
-          </select>
-        </div>
-        <div>
-          <span>To: </span>
-          <select>
-            <option value='USD'>USD</option>
-            <option value='PLN'>PLN</option>
-            <option value='GBP'>GBP</option>
-            <option value='EUR'>EUR</option>
-          </select>
-        </div>
-      </form>
+            <div>
+                <input type='number' placeholder='Amount' onChange={
+                    (event) => {
+                        setAmount(event.target.value);
+                    }}
+                />
+            </div>
+            <div>
+                <span>From: </span>
+                    <Select value={currencyFrom} setCurrency={setCurrencyFrom}/>
+            </div>
+            <div>
+                <span>To: </span>
+                <Select value={currencyTo} setCurrency={setCurrencyTo}/>
+            </div>
+            <div>
+                Result: {amount}
+            </div>
+        </form>
     );
 }
 
